63ff8f6d341cac46913c5fe39f7e107e02c4f623
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-08-06 13:48:02 +0800

    Fix namespace package path

---
+++ olive/devices/camera.py
@@ -0,0 +1,19 @@
+
+class Camera():
+    def __init__(self):
+        pass
+
+    # High level functions
+    #   - Snap: Capture all or a portion of a single image to the user buffer
+    #   - Grab: Perform an acquisition that loops continually on one or more internal buffers.
+    #   - Sequence: Aquire a specified number of internal buffers and then stops.
+    def func(self):
+        pass
+    
+    # Low level functions
+    #   - Acquisition: Configure, start, stop, and unconfigure an image acquisition.
+    #   - Attribute: Examine and change the acquisition or camera attributes.
+    #   - Event: Register events.
+    #   - Register: Access registers.
+    #   - Session: Open, close, or enumerate sessions.
+    #   - Utility: Get detailed error information.
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='olive-core',
+    version='1',
+
+    description='',
+    long_description='',
+
+    author='Liu, Yen-Ting', 
+    author_email='',
+
+    packages=['olive'],
+    #namespace_packages=['olive.devices.drivers'],
+    zip_safe=False
+)
