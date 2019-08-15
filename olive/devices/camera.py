02c12d851cef9c1e798d9d44f822ee34f0732d6c
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-08-15 12:18:07 +0800

    Update camera base class definitions

--- olive/devices/camera.py
+++ olive/devices/camera.py
@@ -1,5 +1,29 @@
+from abc import ABCMeta, abstractmethod
+import logging
+from typing import NamedTuple
 
-class Camera():
+__all__ = ['Camera', 'CameraInfo']
+
+
+class CameraInfo(NamedTuple):
+    #: version of the API
+    version : str
+    #: vendor name of the camera
+    vendor : str
+    #: modle name of the camera
+    model : str
+    #: camera serial number
+    serial_number : str
+    
+    def __repr__(self) -> str:
+        return f'<CameraInfo {self.vendor}, {self.model}, S/N={self.serial_number}>'
+
+class Attribute(NamedTuple):
+    readable : bool
+    writable : bool
+    name : str
+    
+class Camera(metaclass=ABCMeta):
     def __init__(self):
         pass
 
@@ -7,13 +31,90 @@ class Camera():
     #   - Snap: Capture all or a portion of a single image to the user buffer
     #   - Grab: Perform an acquisition that loops continually on one or more internal buffers.
     #   - Sequence: Aquire a specified number of internal buffers and then stops.
-    def func(self):
+    def snap(self):
         pass
     
+    def configure_grab(self):
+        pass
+    
+    def grab(self):
+        pass
+    
+    def sequence(self):
+        pass
+
+    def open_camera(self):
+        pass
+
+    def close_camera(self):
+        pass
+    
+    @abstractmethod
+    def enumerate_cameras(self):
+        """Returns a list of supported camera hardwares."""
+        pass
+    
+    def enumerate_attributes(self):
+        """Gets the attributes supported by the camera."""
+        pass 
     # Low level functions
     #   - Acquisition: Configure, start, stop, and unconfigure an image acquisition.
+    def configure_acquisition(self):
+        pass
+
+    def start_acquisition(self): 
+        """
+        Starts an acquisition.
+
+        Use stop_acquisition() to stop the acquisition.
+        """  
+        pass
+
+    def get_image(self):
+        """Get data of the specified frame."""
+        pass
+    
+    def stop_acquisition(self):
+        """Stops an acquistion."""
+        pass 
+    
+    def unconfigure_acquisition(self):
+        pass
     #   - Attribute: Examine and change the acquisition or camera attributes.
+    def read_attributes(self):
+        pass
+    
+    def write_attributes(self):
+        pass
     #   - Event: Register events.
     #   - Register: Access registers.
     #   - Session: Open, close, or enumerate sessions.
     #   - Utility: Get detailed error information.
+
+    """
+     Snap2
+     Configure Grab
+     Grab2
+     Sequence2
+     Open Camera
+     Close Camera
+     Enumerate Cameras
+     Enumerate Attributes
+    Enumerate Video Modes
+    
+    --- low level ---
+     Configure Acquisition
+    Configure Ring Acquisition
+     Start Acquisition
+     Stop Acquisition
+     Unconfigure Acquisition
+     Get Image2
+     (Get Image Data)
+    Read Register2
+    Write Register2
+    Read Memory2
+    Write Memory2
+    Read Attributes
+    Write Attributes
+    Register for Events
+    """
