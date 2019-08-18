c5c88cad71b003c6beea97b1d346798fcc95e966
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-08-18 13:29:03 +0800

    Update setup.py package info

--- setup.py
+++ setup.py
@@ -1,16 +1,35 @@
+import os
+
 from setuptools import find_packages, setup
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(cwd, "README.md"), encoding='utf-8') as fd:
+    LONG_DESCRIPTION = fd.read()
 
 setup(
     name='olive-core',
     version='1',
 
-    description='',
-    long_description='',
+    description='Open LIVE microscopy',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/liuyenting/olive-core',
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License'
+    ]},
+    keywords=[],
 
     author='Liu, Yen-Ting', 
-    author_email='',
+    author_email='ytliu@gate.sinica.edu.tw',
+
+    # use pyproject.toml for setup dependencies instead
+    #setup_requires=[],
+    install_requires=[],
 
     packages=find_packages(),
-    #namespace_packages=['olive.drivers'],
+    namespace_packages=['olive.drivers'],
+
     zip_safe=False
 )
