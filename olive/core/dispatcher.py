ca386b58255c5c7e3c07beb794952eb9c545c0b5
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-08-18 15:17:30 +0800

    Add core components

---
+++ olive/core/dispatcher.py
@@ -0,0 +1,8 @@
+class Dispatcher(object):
+    """
+    Dispatch device confiugrations and formulate the sequence to execute on a sequencer.
+    """
+
+    def __init__(self):
+        pass
+
---
+++ olive/core/sequence.py
@@ -0,0 +1,7 @@
+class Sequence(object):
+    """
+    Define system control sequence.
+    """
+
+    def __init__(self):
+        pass
