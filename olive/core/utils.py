b9f4440226ad5cdb87f404747ffab35aa8b86748
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-08-26 07:36:21 +0800

    Reorder log position

--- olive/core/utils.py
+++ olive/core/utils.py
@@ -10,8 +10,6 @@ class Singleton(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            logger.debug(
-                'creating new singleton object "{}"'.format(type(cls).__name__)
-            )
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            logger.debug('new singleton object "{}" created'.format(cls))
         return cls._instances[cls]
