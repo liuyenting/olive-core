7a39f41ce50c3dab1e14520e0fff035f581fc4f0
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-08-28 00:05:37 +0800

    Add motion stages interface

--- olive/devices/motion.py
+++ olive/devices/motion.py
@@ -1,8 +1,9 @@
+from abc import abstractmethod
 import logging
 
 from olive.core import Device
 
-__all__ = ["Galvo"]
+__all__ = ["Galvo", "Stage", "LinearStage", "RotaryStage", "MotionController"]
 
 logger = logging.getLogger(__name__)
 
@@ -17,15 +18,31 @@ class Galvo(Device):
 
 
 class Stage(Device):
+    @abstractmethod
+    def __init__(self, driver, parent):
+        super().__init__(driver, parent)
+
+
+class LinearStage(Stage, Device):
+    """
+    Linear stage is a component of a precise motion system used to restrict an object
+    to a single axis of _translation_.
+    """
+
+    @abstractmethod
+    def __init__(self, driver, parent):
+        super().__init__(driver, parent)
+
+
+class RotaryStage(Stage, Device):
+    """
+    A rotary stage is a component of a motion system used to restrict an object to a single axis of _rotation_.
+    """
+
+    @abstractmethod
+    def __init__(self, driver, parent):
+        super().__init__(driver, parent)
+
+
+class MotionController(Device):
     pass
-
-
-class LinearStage(Stage):
-    pass
-
-
-class RotaryStage(Stage):
-    pass
-
-
-# TODO difference between stage controller and axes, use parent
