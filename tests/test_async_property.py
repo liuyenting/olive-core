29152bafec6506427efbd4e0f5601ba167a4bcf3
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-03-07 18:36:33 +0800

    Add async set/get

--- tests/test_async_property.py
+++ tests/test_async_property.py
@@ -1,26 +1,50 @@
-import functools
 import asyncio
-from dataclasses import dataclass
+import functools
+import logging
 import typing
+from dataclasses import dataclass
 
-ASYNC_PROPERTY_ATTR = "__device_property_cache__"
+from olive.devices.error import DeviceError
+
+DEVICE_PROPERTY_CACHE_ATTR = "__device_property_cache__"
+
+logger = logging.getLogger(__name__)
+
+
+class CacheError(DeviceError):
+    pass
+
+
+class CacheMiss(CacheError):
+    pass
+
+
+class DirtyCacheError(CacheError):
+    pass
 
 
 @dataclass
 class DevicePropertyCache:
-    cache: typing.Any = None
+    value: typing.Any = None
     lock: asyncio.Lock = asyncio.Lock()
-    dirty: bool = False
+    dirty: bool = True
 
 
 class DevicePropertyDescriptor:
     """
     This class builds a data descriptor that provides async read-write access for
     low-level hardwares, and annotate them with proper signatures.
+
+    Args:
+        fget
+        fset
+        volatile (bool, optional): property may change at any-time, access cache is not
+            trustworthy
     """
 
-    def __init__(self, fget=None, fset=None, doc=None):
+    def __init__(self, fget=None, fset=None, volatile=False):
         self._fget, self._fset = fget, fset
+        self._volatile = volatile
 
         # update wrapper using the getter/setter function
         functools.update_wrapper(self, fget if fget is not None else fset)
@@ -38,7 +62,7 @@ class DevicePropertyDescriptor:
             return self
         if self._fget is None:
             raise AttributeError("this property is not readable")
-        return self._fget(instance)
+        return self._get_cache_value(instance)
 
     def __set__(self, instance, value):
         """
@@ -50,66 +74,154 @@ class DevicePropertyDescriptor:
         """
         if self._fset is None:
             raise AttributeError("this property is not writable")
-        self._fset(instance, value)
+        self._set_cache_value(instance, value)
+        # self._fset(instance, value)
 
     def __delete__(self, instance):
         raise AttributeError("device property is not deletable")
 
     ##
-    # alternative constructors
+    # alternative constructor to attach setter
 
     def getter(self, fget):
-        return type(self)(fget, self._fset, self.__doc__)
+        self._check_method_name(fget, self._fset, "getter")
+        return type(self)(fget, self._fset, self._volatile)
 
     def setter(self, fset):
-        return type(self)(self._fget, fset, self.__doc__)
+        self._check_method_name(fset, self._fget, "setter")
+        return type(self)(self._fget, fset, self._volatile)
+
+    def _check_method_name(self, new_method, current_method, method_type):
+        assert (
+            new_method.__name__ == current_method.__name__
+        ), f"@{new_method.__name__}.{method_type} name must match property name"
 
     ##
-    #
+    # cache access
 
-    async def sync(self):
-        """Sync the property with device."""
-        # TODO trigger fset
-        pass
+    def _get_cache(self, instance):
+        """Get cache attribute."""
+        try:
+            return getattr(instance, DEVICE_PROPERTY_CACHE_ATTR)
+        except AttributeError:
+            raise CacheMiss()
+            logger.debug("cache missed, starts as dirty cache")
+            cache = DevicePropertyCache(dirty=True)
+            setattr(instance, DEVICE_PROPERTY_CACHE_ATTR, cache)
+            return cache
+
+    async def _get_cache_value(self, instance):
+        """
+        Extract cache value from the cache attribute.
+
+        Cache attribute is temporarily locked during the process to ensure reading is
+        atomic.
+
+        Args:
+            instance (object): the instance to operate on
+        """
+        try:
+            cache = self._get_cache(instance)
+        except CacheMiss:
+            logger.debug(f"cache missed when getting {instance}")
+            value = await self._fget(instance)
+            cache = DevicePropertyCache(value=value, dirty=False)
+            setattr(instance, DEVICE_PROPERTY_CACHE_ATTR, cache)
+        else:
+            # we might have not sync yet
+            async with cache.lock:
+                if cache.dirty:
+                    raise DirtyCacheError()
+                else:
+                    value = cache.value
+        return value
+
+    def _set_cache_value(self, instance, value):
+        """
+        Store cache value to the cache attribute.
+
+        When cache is modified, caller has to use `sync` to ensure cache is valid with the device.
+
+        Args:
+            instance (object): the instance to operate on
+            value : the new value
+        """
+        try:
+            cache = self._get_cache(instance)
+            cache.value, cache.dirty = value, True
+        except CacheMiss:
+            logger.debug(f"cache missed when setting {instance}")
+            cache = DevicePropertyCache(value=value, dirty=True)
+            setattr(instance, DEVICE_PROPERTY_CACHE_ATTR, cache)
+
+    # TODO how to link sync when __get__
+    async def sync(self, instance):
+        """Sync the device property with device."""
+        cache = self._get_cache(instance)
+        if cache.dirty:
+            async with cache.lock:
+                logger.debug("updating cache")
+                await self._fset(instance, cache.value)
+                cache.dirty = False
 
 
-class ReadOnlyDevicePropertyDescriptor(DevicePropertyDescriptor):
-    pass
+def rw_property(func, *args, **kwargs):
+    """
+    A readable and writable device property. You can annotate with getter and assign
+    setter later, or vice versa.
 
-
-class WriteOnlyDevicePropertyDescriptor(DevicePropertyDescriptor):
-    pass
-
-
-def device_property(func, *args, **kwargs):
+    Args:
+        func (TBA): TBA
+    """
     assert asyncio.iscoroutinefunction(func), "can only use with async def"
     return DevicePropertyDescriptor(func, *args, **kwargs)
 
 
+def ro_property():
+    pass
+
+
+def wo_property():
+    pass
+
+
 class MyObject(object):
     def __init__(self):
-        self.val = 0
+        self._val = 0
 
-    @device_property
+    @rw_property
     async def val(self):
         print("getting")
         await asyncio.sleep(1)
-        return self.val
+        return self._val
 
     @val.setter
     async def val(self, new_val):
         print("setting")
-        await asyncio.sleep(1)
-        # self.val += new_val
+        await asyncio.sleep(2)
+        self._val += new_val
 
 
 async def main():
     obj = MyObject()
+
     val = await obj.val
     print(val)
 
+    obj.val = 42
+    val = await obj.val
+    print(val)  # dirty cache exception
+
+    print(dir(obj.val))
+
 
 if __name__ == "__main__":
+    import coloredlogs
+
+    coloredlogs.install(
+        level="DEBUG", fmt="%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S"
+    )
+
     # launch generic loop
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
